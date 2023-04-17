160ffc2e033575decf9a9075231f49033587aaf5
RupeshSahu <75201337+RupeshSahu969@users.noreply.github.com>
2023-04-18 00:22:34 +0530

    add update

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-32x3cRV5MukiNbql3tumKldWtF19DU8",
-  authDomain: "recat-firebase-auth-c2dc0.firebaseapp.com",
-  projectId: "recat-firebase-auth-c2dc0",
-  storageBucket: "recat-firebase-auth-c2dc0.appspot.com",
-  messagingSenderId: "563206552068",
-  appId: "1:563206552068:web:da5ac30074d21415495351"
+  apiKey: <YOURAPIKEY>,
+  authDomain: <YOURAUTHDOAMIN>,
+  projectId: <YOURPROJECTID>,
+  storageBucket: <YOURSTORAGEBUCKET>,
+  messagingSenderId: <YOURMESSAGINGSENDERID>,
+  appId: <YOURAPPID>,
 };
 
 
